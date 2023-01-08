56dba7f129dfe7f8e774142c9a5f2701ff4616bb
dmuruges <sekar.murugesan@gmail.com>
2023-01-08 15:41:43 +0530

    Modified 06-aks-administrators-azure-ad.tf

--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -2,5 +2,5 @@
 resource "azuread_group" "aks_administrators" {
   display_name = "${azurerm_resource_group.aks_rg.name}-administrators"
   security_enabled = true
-  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-cluster"
+  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-administrators"
 }
