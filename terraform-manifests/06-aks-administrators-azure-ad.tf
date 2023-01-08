be76f7e49ef97700dec31ac25981fddac6e2d6f4
dmuruges <sekar.murugesan@gmail.com>
2023-01-08 15:05:21 +0530

    Third Commit - Dev AKS Provision via terraform

--- terraform-manifests/06-aks-administrators-azure-ad.tf
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -1,6 +1,6 @@
 
 resource "azuread_group" "aks_administrators" {
-  display_name = "${azurerm_resource_group.aks_rg.name}-${var.environment}"
+  display_name = "${azurerm_resource_group.aks_rg.name}-administrators"
   security_enabled = true
   description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-cluster"
 }
